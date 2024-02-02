4e0e098c82dda230e8cc9bd857d1c730cdf4bdf7
Abner da Silva Santos <93294372+AbnerSantos25@users.noreply.github.com>
2024-02-02 10:50:08 -0300

    c#

---
+++ ProjetoAulas/Program.cs
@@ -0,0 +1,40 @@
+﻿
+// Console.WriteLine("teste");
+// const string constante = "Constante 1";
+// Console.WriteLine(constante);
+
+// int numero1 = 1;
+// Console.WriteLine(numero1);
+
+// double numero2 = 2;
+// Console.WriteLine(numero2);
+
+// float numero3 = 3;
+// Console.WriteLine(numero3);
+
+// decimal numero4 = 4;
+// Console.WriteLine(numero4);
+
+// bool VF = true;
+// Console.WriteLine(VF);
+
+var nome = NomeCompleto();
+EscreverNome();
+
+void EscreverNome()
+{
+    var nome = NomeCompleto();
+
+    Console.WriteLine(nome);
+}
+
+string NomeCompleto()
+{
+    string primeiroNome = "Rafael";
+    string segundoNome = "Almeida";
+    return primeiroNome + " " + segundoNome;
+}
+
+
+
+
